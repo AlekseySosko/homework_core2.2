a9ccc11a08a1ac65d2bd2ca2d76e19100c4db301
Sosko Aleksey <sosko.aleksey@gmail.com>
2022-03-20 17:47:17 +0700

    core2.2

---
+++ Education.java
@@ -0,0 +1,8 @@
+package ru.netology.core4;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ Sex.java
@@ -0,0 +1,6 @@
+package ru.netology.core4;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
